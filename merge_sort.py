163770d9dbc397895837107f54dba6d0ea8b8ced
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-10-31 23:51:33 +0530

    code added

--- merge_sort.py
+++ merge_sort.py
@@ -24,29 +24,44 @@ Merging:
 
 """
 
-def merge_sort(nums):
-
-
 def merge_array(left, right):
     result=[]
     i,j=0,0
     n,m=len(left),len(right)
     while(i<n and j<m):
-        if left[i]<=right[j]:
+        if (left[i]<=right[j]):
             result.append(left[i])
             i+=1
         else:
             result.append(right[j])
             j+=1
-
-    if i<n:
-        while i<n:
+    if (i<n):
+        while (i<n):
             result.append(left[i])
-    if j<m:
-        while j<m:
+            i+=1
+    if (j<m):
+        while (j<m):
             result.append(right[j])
             j+=1
-    
     return result
 
+
+
+def merge_sort(nums):
+    if len(nums)<=1:
+        return nums
+    mid = len(nums)//2
+    left_arr = nums[:mid]
+    right_arr = nums[mid:]
+    
+    left = merge_sort(left_arr)
+    right = merge_sort(right_arr)
+
+    return merge_array(left,right)
+
+
 nums = [3,1,2,4,1,5,2,6,4]
+print("Unsorted Array: ",nums)
+
+print("**Merge Sort Applied**")
+print("Sorted Array: ",merge_sort(nums))
