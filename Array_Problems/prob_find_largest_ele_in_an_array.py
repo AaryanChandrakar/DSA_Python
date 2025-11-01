bd24207faa7a780519ae83cc70075f5d6d885bc9
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-01 17:55:28 +0530

    added method 2

--- Array_Problems/prob_find_largest_ele_in_an_array.py
+++ Array_Problems/prob_find_largest_ele_in_an_array.py
@@ -4,7 +4,7 @@ nums = [55,32,-97,99,3,67]
 
 # Method: 1
 def fun(nums):
-    largest = 0
+    largest = nums[0]
     for i in nums:
         if i>=largest:
             largest = i
@@ -13,5 +13,20 @@ def fun(nums):
 largest_ele = fun(nums)
 print("Array: ",nums)
 print("Largest Element in Array is: ",largest_ele)
+#  Time Complexity: 0(n)
+# Space Complexity: 0(1)
+
+
+# Method: 2
+def fun_inf(nums):
+    largest = float("-inf")
+    for i in nums:
+        if i>=largest:
+            largest = i
+    return largest
+    
+largest_ele_inf = fun_inf(nums)
+print("Array: ",nums)
+print("Largest Element in Array is: ",largest_ele_inf)
 #  Time Complexity: 0(n)
 # Space Complexity: 0(1)
