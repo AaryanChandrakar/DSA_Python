a7d13d587e38abd1192b17e9af2ce83ac472d801
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-04 23:40:42 +0530

    code added

--- Array_Problems/array_rotation.py
+++ Array_Problems/array_rotation.py
@@ -9,3 +9,17 @@ arr = [7, 4, 2, 10, 5, 3, 1, 6]
 right_rotate_by_one(arr)
 print(arr)   # Output: [6, 7, 4, 2, 10, 5, 3, 1]
 
+# --------------------------------
+
+# Approach 2: Without slicing (using a loop)
+def right_rotate_by_one_loop(nums):
+    n = len(nums)
+    temp = nums[-1]
+    for i in range(n - 2, -1, -1):
+        nums[i + 1] = nums[i]
+    nums[0] = temp
+
+# Example usage:
+arr2 = [7, 4, 2, 10, 5, 3, 1, 6]
+right_rotate_by_one_loop(arr2)
+print(arr2)  # Output: [6, 7, 4, 2, 10, 5, 3, 1]
