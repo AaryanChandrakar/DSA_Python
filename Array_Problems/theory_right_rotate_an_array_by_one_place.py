719685440fddfd52aac9eac191f7441b9d0ce791
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-06 23:39:44 +0530

    code added

--- Array_Problems/theory_right_rotate_an_array_by_one_place.py
+++ Array_Problems/theory_right_rotate_an_array_by_one_place.py
@@ -37,13 +37,32 @@ print(arr2)  # Output: [6, 7, 4, 2, 10, 5, 3, 1]
 
 # Right Rotate an Array by 1 Place 
 
-# Using Slicing [ : ]
+# 1. Using Slicing [ : ]
 #index  0   1  2  3  4  5   6  7
 nums = [5, -2, 3, 9, 0, 6, 10, 7]
 #index -8  -7 -6 -5 -4 -3  -2 -1
 n = len(nums)
-print("Before Rottion: ",nums)
+print("Before Rotation: ",nums)
 nums[:] = [nums[-1]] + nums[0:n-1]
 # Or      [nums[n-1]] + nums[0:n-1]
-print("After Rotation: ",nums)
+print("After Rotation:  ",nums)
+
+#  Time Complexity: 0(n)
+# Space Complexity: 0(n)
+
+
+
+# 2. Using Loop:
+def fun (nums):
+    n = len(nums)
+    temp = nums[n-1]
+    for i in range(n-2,-1,-1):
+        nums[i+1] = nums[i]
+    nums[0] = temp
+    return nums
+
+print("Before Rotation: ",nums)
+print("After Rotation:  ",fun(nums))
+#  Time Complexity: 0(n)
+# Space Complexity: 0(1)
 
