aec9f056fd53f82a05d12fede7d316af93f426cc
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-09 20:46:26 +0530

    code added

---
+++ Array_Problems/prob_max_consecutive_ones.py
@@ -0,0 +1,17 @@
+# Max Consecutive Ones (in binaru array)
+
+nums = [1,1,0,1,0,1,1,1,1,0,1,1]   #output: 4
+
+def max_consicutive_ones(nums):
+    count = 0
+    max_count = 0
+    for i in nums:
+        if i == 1:
+            count+=1
+            if count > max_count:
+                max_count = count
+        else:
+            count = 0
+    return max_count
+print("Max Consicutive Ones",max_consicutive_ones(nums))
+
