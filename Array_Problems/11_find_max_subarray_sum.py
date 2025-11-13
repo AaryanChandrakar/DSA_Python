3c44c40a1fc6ad4cca147f225d79ebb0cf6daf27
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-13 21:57:33 +0530

    code added

--- Array_Problems/11_find_max_subarray_sum.py
+++ Array_Problems/11_find_max_subarray_sum.py
@@ -9,7 +9,7 @@ if we are preparing array by picking non-continuos elements, then it is called '
 """
 
 # nums = [-2,1,-3,4,-1,2,1,-5,4]
-
+# Normal Approach
 def fun(nums):
     n = len(nums)
     max_sum_arr = float("-inf")
@@ -21,7 +21,25 @@ def fun(nums):
     return max_sum_arr
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 print("Array: ",nums)
-print("Max Sum Array: ",fun(nums))
+print("Max Sub-Array Sum: ",fun(nums))
 # Time Complexity:  O(n^2)
 # Space Complexity: O(1)
+print("-----------------------------------")
+
+
+#Kadans Algorithm (Optimal)
+def find_max_subarr(nums):
+    n = len(nums)
+    maxi = float("-inf")
+    total = 0
+    for i in range(0,n):
+        total = total + nums[i]
+        maxi = max(maxi, total)
+        if total<0:
+            total = 0
+    return maxi
+print("Maximum Sub-Array Sum: ",find_max_subarr(nums))
+# Time Complexity:  O(n)
+# Space Complexity: O(1)
+
 
