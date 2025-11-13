52c8ed90b4354270e6a028aae9330d9475a3103f
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-13 20:06:31 +0530

    code added

--- Array_Problems/11_find_max_subarray_sum.py
+++ Array_Problems/11_find_max_subarray_sum.py
@@ -8,14 +8,20 @@ elements of sub-array must be continuos in the original array.
 if we are preparing array by picking non-continuos elements, then it is called 'sub-sequence'.
 """
 
-# nums = [-2,1,-3,4,-1,2,-5,4]
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
 
 def fun(nums):
     n = len(nums)
-    max_sum_arr = []
-
-
-
-nums = [-2,1,-3,4,-1,2,-5,4]
+    max_sum_arr = float("-inf")
+    for i in range(0,n):
+        total = 0
+        for j in range(i,n):
+            total = total + nums[j]
+            max_sum_arr = max(max_sum_arr,total)
+    return max_sum_arr
+nums = [-2,1,-3,4,-1,2,1,-5,4]
 print("Array: ",nums)
 print("Max Sum Array: ",fun(nums))
+# Time Complexity:  O(n^2)
+# Space Complexity: O(1)
+
