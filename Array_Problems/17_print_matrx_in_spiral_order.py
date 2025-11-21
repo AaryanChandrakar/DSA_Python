89bc781ff2228a6eff97ad10d7d509d7d20512cb
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-22 00:23:01 +0530

    code updated

--- Array_Problems/17_print_matrx_in_spiral_order.py
+++ Array_Problems/17_print_matrx_in_spiral_order.py
@@ -8,7 +8,6 @@ def spiral_order(nums):
         return []
     result = []
 
-    # Initialize poiter for traversal.
     top, left = 0, 0
     bottom, right = len(nums)-1, len(nums[0])-1
 
@@ -33,36 +32,7 @@ def spiral_order(nums):
             left+=1
     return result
 print("Spiral Order: ",spiral_order(nums))
-def spiral_order(nums):
-    if not nums or not nums[0]:
-        return []
-    result = []
 
-    # Initialize poiter for traversal.
-    top, left = 0, 0
-    bottom, right = len(nums)-1, len(nums[0])-1
-
-    while top<=bottom and left<=right:
-
-        for i in range(left, right+1):
-            result.append(nums[top][i])
-        top+=1
-
-        for i in range(top, bottom+1):
-            result.append(nums[i][right])
-        right-=1
-
-        if top<=bottom:
-            for i in range(right, left-1, -1):
-                result.append(nums[bottom][i])
-            bottom-=1
-
-        if left<=right:
-            for i in range(bottom, top-1, -1):
-                result.append(nums[i][left])
-            left+=1
-    return result
-print("Spiral Order: ",spiral_order(nums))
 # Time Complexity:  O(n*m)
 # Space Complexity: O(1)
 
