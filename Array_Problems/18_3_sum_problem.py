569bb3f5ce0cf632a77bd4850cb1aeebb7691a13
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-23 23:02:42 +0530

    optimal solution added

--- Array_Problems/18_3_sum_problem.py
+++ Array_Problems/18_3_sum_problem.py
@@ -2,36 +2,35 @@
 # have to pick 3 element (non-repeating elements), 
 # and the sum of those 3 element must be equal to the given number
 
-# nums = [-1,0,1,2,-1,-4] , x=0
+# nums = [-1,0,1,2,-1,-4] ,
 """
-The best pick can be [-1,2,-1] , sum=0 == x=0
-                     [0,1,-1]  , sum=0 == x=0
+The best pick can be [-1,2,-1] , sum==0
+                     [0,1,-1]  , sum==0
 
 We need to return 2d list/array: [[1-,2,-1],[0,1,-1]]
 We do not need to return result in order
 
 """
+nums = [-1,0,1,2,-1,-4]
 
 # Bruteforce Approach
 
-def fun_bruteforce(nums,x):
+def fun_bruteforce(nums):
      n = len(nums)
      my_set = set()
      for i in range(0,n):
           for j in range(i+1,n):
                for k in range(j+1,n):
-                    if nums[i]+nums[j]+nums[k]==x:
+                    if nums[i]+nums[j]+nums[k]==0:
                          temp = [nums[i],nums[j],nums[k]]
                          temp.sort()
                          my_set.add(tuple(temp))
      return [list(ans) for ans in my_set]
      
-nums = [-1,0,1,2,-1,-4]
-x=0
-print("Result: ",fun_bruteforce(nums,x))
+print("Result: ",fun_bruteforce(nums))
 # Time Complexity:  O(n^3)
 # Space Complexity: O(no. of triplets)
-print("--------------------------------------")
+print("--------------------------------------------")
 
 # Better Approach:
 
@@ -57,5 +56,30 @@ print("--------------------------------------------")
 """
 Time Complexity can not be reduced, but we can reduce Space Complexity
 """
+def threeSum_opt(nums):
+        n = len(nums)
+        result = []
+        nums.sort()
+        for i in range(n-2):
+            if i != 0 and nums[i]==nums[i-1]:
+                continue
+            j =i+1
+            k =n-1
+            while j<k:
+                total_sum = nums[i]+nums[j]+nums[k]
+                if total_sum < 0:
+                    j+=1
+                elif total_sum > 0:
+                    k-=1
+                else:
+                    temp = [nums[i],nums[j],nums[k]]
+                    result.append(temp)
+                    j+=1
+                    k-=1
 
-     
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:
+                        k-=1
+        return result
+print("Result: ",threeSum_opt(nums))
