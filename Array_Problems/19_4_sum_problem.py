a125e92303b52324268aff86437a4bc5d424ef70
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-11-24 10:16:20 +0530

    code updated

--- Array_Problems/19_4_sum_problem.py
+++ Array_Problems/19_4_sum_problem.py
@@ -16,18 +16,26 @@ target = 0
 # Bruteforce Approach
 def fun_brute(nums, target):
     n = len(nums)
-    result = []
+    my_set = set()
+    if n<4:
+        return []
     for i in range(0,n):
         for j in range(i+1,n):
             for k in range(j+1,n):
                 for l in range(k+1,n):
                     sum = nums[i]+nums[j]+nums[k]+nums[l]
                     if sum == target:
-                        temp = [nums[i],nums[j],nums[k],nums[l]]
-                        result.append(sorted(temp))
+                        my_set.add(tuple(sorted([nums[i],nums[j],nums[k],nums[l]])))
+                        
+    result = []
+    for i in my_set:
+        result.append(list(i))
     return result
 print("Result: ",fun_brute(nums,target))
 # Time Complexity:  O(n^4)
 # Space Complexity: O(no. of quadruplet)
+print("-------------------------------------------------------------")
 
-
+# Better Approach
+def fun_better(nums,traget):
+    n = len(nums)
