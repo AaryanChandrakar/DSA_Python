a60d74b017e6dfe53b4cabc6c4de1f73de4e25c8
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-12-02 00:07:44 +0530

    code added

---
+++ Binary_Search/lowerBound_upperBound.py
@@ -0,0 +1,34 @@
+# Lower Bound: Smallest index such that nums[i]>=target
+# Upper Bound: Larget index such that nums[i]<=tareget
+
+nums = [1,1,1,2,3,3,5,6,7,7,7,9,12,12,13]
+
+n = len(nums)
+lb = n
+low = 0, high = n-1
+while low<high:
+    mid = (low+high) // 2
+    if nums[mid] >= target:
+        lb = mid
+        high = mid-1
+    else:
+        low = mid+1
+return lb
+
+
+
+
+n = len(nums)
+ub = n
+low = 0, high=n-1
+while low<=high:
+    mid=(low+high)//2
+    if nums[mid]>target:
+        ub=mid
+        high=mid-1
+    else:
+        low=mid+1
+return ub
+
+
+# Time Complexity: O(log2(n))
