ed5f777347e4152e27b73e1967702788fda90244
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-12-03 09:47:32 +0530

    code added

--- Binary_Search/lowerBound_upperBound.py
+++ Binary_Search/lowerBound_upperBound.py
@@ -1,36 +1,42 @@
 # Lower Bound: Smallest index such that nums[i]>=target
 # Upper Bound: Larget index such that nums[i]<=tareget
 
-nums = [1,1,1,2,3,3,5,6,7,7,7,9,12,12,13]
+nums = [1,1,1,2,3,3,5,6,7,7, 7, 9, 12, 12, 13]
+#index  0 1 2 3 4 5 6 7 8 9 10 11  12  13  14
+target = 7
 
-"""
-n = len(nums)
-lb = n
-low = 0, high = n-1
-while low<high:
-    mid = (low+high) // 2
-    if nums[mid] >= target:
-        lb = mid
-        high = mid-1
-    else:
-        low = mid+1
-return lb
-"""
+def lower_bound(nums, target):
+    n = len(nums)
+    lb = n
+    low = 0
+    high = n-1
+    while low<=high:
+        mid = (low+high) // 2
+        if nums[mid] >= target:
+            lb = mid
+            high = mid-1
+        else:
+            low = mid+1
+    return lb
+print("Lower Bound: ",lower_bound(nums,target))
+# Time Complexity:  O(log2(n))
+# Space Complexity: O(1)
 
 
 
-"""
-n = len(nums)
-ub = n
-low = 0, high=n-1
-while low<=high:
-    mid=(low+high)//2
-    if nums[mid]>=target:
-        ub=mid
-        high=mid-1
-    else:
-        low=mid+1
-return ub
-
-# Time Complexity: O(log2(n))
-"""
+def upper_bound(nums, target):
+    n = len(nums)
+    ub = n
+    low = 0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]>target:
+            ub=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ub
+print("Upper Bound: ",upper_bound(nums,target))
+# Time Complexity:  O(log2(n))
+# Space Complexity: O(1)
