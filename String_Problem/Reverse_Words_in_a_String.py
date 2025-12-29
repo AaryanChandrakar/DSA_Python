8268e52df82f0554a5128e02d9899e5bf7758ccf
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-12-29 09:19:55 +0530

    code added

---
+++ String_Problem/Reverse_Words_in_a_String.py
@@ -0,0 +1,12 @@
+# Reverse the words in a given string.
+
+# Way 1: Using built-in functions
+def reverse_words(s):
+    words = s.split()
+    reversed_words = words[::-1]
+    return ' '.join(reversed_words)
+
+s = "  Hello   World  "
+print(f'Input:  "{s}"')
+print(f'Output: "{reverse_words(s)}"')
+print('---')
