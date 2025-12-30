340ed18a591102adbeacc4042d62c5a27b6c62e6
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-12-30 12:07:30 +0530

    code added

---
+++ String_Problem/3_Largest Odd_Number_in_String.py
@@ -0,0 +1,11 @@
+def larget_oddNuber(s):
+    n = len(s)
+    for i in range(n-1, -1, -1):
+        if int (s[i]) % 2 == 1:
+            return s[:i+1]
+    return ""   
+test_cases = ["52", "4206", "35427", "1234", "2468", "9876543210"]
+for s in test_cases:
+    print(f'Input:  {s}')
+    print(f'Output: {larget_oddNuber(s)}')
+    print('---')
