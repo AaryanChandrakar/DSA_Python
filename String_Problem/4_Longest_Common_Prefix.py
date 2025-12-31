c97a9862c1a57a46937eb3b81b8636c1e4af985a
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-12-31 23:44:13 +0530

    code added

---
+++ String_Problem/4_Longest_Common_Prefix.py
@@ -0,0 +1,19 @@
+# Longest Common Prefix in the given array of string
+# This problem can be solved using vertical scanning
+
+def fun(strs):
+    if len(strs)==0:
+        return ""
+    
+    result = ""
+    base = strs[0]
+    for i in range(0,len(base)):
+        for word in strs[1:]:
+            if i==len(word) or word[i] != base[i]:
+                return result
+        result += base[i]
+    return result
+
+strs = ["flow","flower","flowers"]
+output = fun(strs)
+print(output)
