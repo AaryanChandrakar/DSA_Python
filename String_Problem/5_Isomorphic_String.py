754fea3500a6e3cb2d8cab56bfc84c3a3b4f4bdb
AaryanChandrakar <aryanchandrakar100@gmail.com>
2026-01-20 12:05:27 +0530

    code added

--- String_Problem/5_Isomorphic_String.py
+++ String_Problem/5_Isomorphic_String.py
@@ -1,30 +1,34 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        # Dictionaries to store the mappings
-        map_s_to_t = {}
-        map_t_to_s = {}
-        
-        for i in range(len(s)):
-            char_s = s[i]
-            char_t = t[i]
-            
-            # Check if there's already a mapping for char_s in map_s_to_t
-            if char_s in map_s_to_t:
-                if map_s_to_t[char_s] != char_t:  # Inconsistent mapping
-                    return False
-            else:
-                map_s_to_t[char_s] = char_t
-            
-            # Check if there's already a mapping for char_t in map_t_to_s
-            if char_t in map_t_to_s:
-                if map_t_to_s[char_t] != char_s:  # Inconsistent mapping
-                    return False
-            else:
-                map_t_to_s[char_t] = char_s
-        
-        return True
+# Problem: Isomorphic Strings
+
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
     
+    mapping_s_to_t = {}
+    mapping_t_to_s = {}
+
+    for i in range(len(s)):
+        char_s = s[i]
+        char_t = t[i]
+
+        if char_s in mapping_s_to_t:
+            if mapping_s_to_t[char_s] != char_t:
+                return False
+        else:
+            mapping_s_to_t[char_s] = char_t
+
+        if char_t in mapping_t_to_s:
+            if mapping_t_to_s[char_t] != char_s:
+                return False
+        else:
+            mapping_t_to_s[char_t] = char_s
+
+    return True
 
 
-# Time Complexity:  O(n)
-# Space Complexity: O(k) , k = size of character set, 26 for lower case alphabet
+print(is_isomorphic("egge", "adda"))  # True
+print(is_isomorphic("foo", "bar"))  # False
+print(is_isomorphic("paper", "title"))  # True
+
+# Time Complexity: O(n), where n is the length of the strings.
+# Space Complexity: O(1), since the character set is fixed (ASCII).
