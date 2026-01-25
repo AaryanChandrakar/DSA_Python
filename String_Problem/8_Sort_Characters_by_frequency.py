339787fe56b5c2934ff18c629b92b6745affc75f
AaryanChandrakar <aryanchandrakar100@gmail.com>
2026-01-25 15:14:24 +0530

    code added

---
+++ String_Problem/8_Sort_Characters_by_frequency.py
@@ -0,0 +1,15 @@
+def sort_characters_by_frequency(s):
+    result = ""
+    hash_map = {}
+    for ch in s:
+        hash_map[ch] = hash_map.get(ch, 0) + 1
+    sorted_hash_map = sorted(hash_map.items(), key=lambda x: x[1], reverse=True)
+    for ch, freq in sorted_hash_map:
+        result += ch * freq
+    return result
+
+print(sort_characters_by_frequency("tree"))
+print(sort_characters_by_frequency("cccaaa"))
+print(sort_characters_by_frequency("Aabb"))
+print(sort_characters_by_frequency("Aaryan"))
+print(sort_characters_by_frequency("hhheeellllggooooeeeeeeeee"))
